9069b774283fbc2faffe10fa77139310323912b6
choiO2system <choi.o2system@gmail.com>
2024-03-04 16:59:57 +0900

    20240304

---
+++ GraphicsEditor/src/GMain.java
@@ -0,0 +1,14 @@
+
+public class GMain {
+
+	public GMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		GMainFrame mainFrame = new GMainFrame();
+		mainFrame.setVisible(true);
+
+	}
+
+}
---
+++ GraphicsEditor/src/GMainFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class GMainFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public GMainFrame() {
+		this.setLocation(200,100);
+		this.setSize(400,600);
+	}
+}
