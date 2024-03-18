4bbbc2f575324869dae9b18f110194f6889e1d1a
chois <chois@DESKTOP-OG399DS>
2024-03-18 10:18:33 +0900

    20240318

--- LMS-A/src/Main.java
+++ LMS-A/src/Main.java
@@ -3,13 +3,25 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println((char)0x07);
-		char token = (char)System.in.read();
-		while(token != 0x0a) {
-			System.out.println(token);
-			token = (char)System.in.read();
+		int number[] = new int[10];
+		int count = 0;
+		int code = System.in.read();
+		while(code != 0x0a) {
+			System.out.println(code);
+			if (count == 0) {
+				number0 = code & 0x0000000F;
+				System.out.println(number0);
+			} else if (count == 1) {
+				number1 = code & 0x0000000F;
+				System.out.println(number1);
+			} else if (count == 2) {
+				number2 = code & 0x0000000F;
+				System.out.println(number2);
+			}
+			count = count + 1;
+			code = System.in.read();
 		}
-		System.out.println(token);
+		int result = number0*100+number1*10+number2*1;
+		System.out.println(result);
 	}
-
 }
