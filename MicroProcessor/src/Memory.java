c6b937b7ce636705cfab477e80c93123d898d901
chois <chois@DESKTOP-OG399DS>
2024-04-01 16:18:36 +0900

    20240401

---
+++ MicroProcessor/src/Memory.java
@@ -0,0 +1,20 @@
+
+public class Memory {
+
+	public Memory() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void initialize() {
+	}
+
+	public int load(int mAR) {
+		// TODO Auto-generated method stub
+		return 10;
+	}
+
+	public void finish() {
+	}
+
+
+}
