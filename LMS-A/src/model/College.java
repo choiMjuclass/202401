432fb01ed0249a351036ff1dafdff8277c4a4d58
chois <chois@Samsung_2024>
2025-11-11 15:56:56 +0900

    20251111

--- LMS-A/src/model/College.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class College {
-
-	public College() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- LMS-A/src/model/Department.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Department {
-
-	public Department() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
